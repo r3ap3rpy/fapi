6541181587a816cf6a68964065bc55abb797e57e
Szabo <dszabo@itron.com>
2021-04-22 11:38:01 +0200

    Initialized repo

---
+++ README.md
@@ -0,0 +1,12 @@
+### Welcome 
+
+This is my tutorial source for the [fastapi](https://fastapi.tiangolo.com) framework in python.
+
+In order to get started you need to have **uvicorn** and **fastapi** installed.
+
+``` bash
+venv corn
+corn/scripts/activate
+pip install uvicorn
+pip install fastapi
+```
---
+++ rbody.py
@@ -0,0 +1,16 @@
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+app = FastAPI()
+
+@app.post("/items/")
+async def createItem(item: Item):
+    return item
